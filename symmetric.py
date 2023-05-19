86f07343ebd5417be2a163b45a79533403d05037
Степан Семыкин <semykinstepan2@gmail.com>
2023-05-19 13:27:25 +0400

    Updated SymmetricSystem

--- symmetric.py
+++ symmetric.py
@@ -51,7 +51,34 @@ class SymmetricSystem:
         try:
             with open(path_encrypted_message, 'w') as f:
                 f.write(encrypted_data)
-            logging.info(f' Message read from file: {path_message}')
+            logging.info(
+                f' Encrypted message write to file: {path_encrypted_message}')
+        except OSError as err:
+            logging.warning(f' Encrypted message was not write\nError:{err}')
+            raise
+
+    def symmetric_decryption(self, key: bytes, path_encrypted_message: str, 
+                             path_decrypted_message: str) -> None:
+        try:
+            with open(path_encrypted_message, 'r') as f:
+                encrypted_message = f.read()
+            logging.info(
+                f' Encrypted message read from file: {path_encrypted_message}')
+        except OSError as err:
+            logging.warning(f' Encrypted message has not read\nError:{err}')
+            raise
+        iv = encrypted_message[:16]
+        encrypted_message = encrypted_message[16:]
+        cipher = Cipher(algorithms.SEED(key), modes.CBC(iv))
+        decryptor = cipher.decryptor()
+        data = decryptor.update(encrypted_message) + decryptor.finalize()
+        unpadder = symmetric_padding.PKCS7(128).unpadder()
+        data = unpadder.update(data) + unpadder.finalize()
+        try:
+            with open(path_decrypted_message, 'w') as f:
+                f.write(data)
+            logging.info(
+                f' Decrypted message write to file: {path_decrypted_message}')
         except OSError as err:
             logging.warning(f' Message has not read\nError:{err}')
             raise
